562d3a5ac8f930914a126dc799b09731b1fdc5d5
monptt <ptt_kr@outlook.jp>
2026-03-17 00:29:22 +0900

    A->A

--- Main.lean
+++ Main.lean
@@ -2,3 +2,9 @@ import LeanStudy
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
+
+def successor (n : Nat) : Nat := n + 1
+
+theorem tautology (A : Prop) : A -> A := by
+  intro h
+  exact h
