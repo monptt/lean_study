158b3701b45329e17579bb21884c53c52b52bebc
monptt <ptt_kr@outlook.jp>
2026-05-21 00:20:19 +0900

    定義を変更

--- Modular/Modular.lean
+++ Modular/Modular.lean
@@ -1,7 +1,7 @@
 /- 整数のmod関連 -/
 
 /-- mod の定義 -/
-def mod_eq (a : Int) (b : Int) (n : Int) : Prop := ∃ k : Int, a = b + n * k
+def mod_eq (a : Int) (b : Int) (n : Int) : Prop := ∃ k : Int, a - b =  n * k
 
 /-- 記法の定義 -/
 notation a "≡" b "mod" n => mod_eq a b n
@@ -19,8 +19,8 @@ theorem modEq_refl (a : Int) (n : Int) : a ≡ a mod n := by
 /-- 対称律 -/
 theorem modEq_symm (a : Int) (b : Int) (n : Int) : (a ≡ b mod n) -> (b ≡ a mod n) := by
   intro h
-  rw [mod_eq] at h
   rw [mod_eq]
+  rw [mod_eq] at h
   sorry
 
 
@@ -28,4 +28,7 @@ theorem modEq_symm (a : Int) (b : Int) (n : Int) : (a ≡ b mod n) -> (b ≡ a m
 /-- 推移律 -/
 theorem modEq_trans (a : Int) (b : Int) (c : Int) (n : Int) : (a ≡ b mod n) -> (b ≡ c mod n) -> (a ≡ c mod n) := by
   intro h1 h2
+  rw [mod_eq]
+  rw [mod_eq] at h1
+  rw [mod_eq] at h2
   sorry
